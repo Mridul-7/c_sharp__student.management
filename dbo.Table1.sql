812b0b08390b2b8fa65cb1b5018018619667a57c
mridul <mridulbhardwajj@gmail.com>
2020-01-16 19:23:43 +0530

    student management system

---
+++ dbo.Table1.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Table]
+(
+	[ID] INT NOT NULL PRIMARY KEY, 
+    [Name] VARCHAR(50) NOT NULL, 
+    [Address] VARCHAR(50) NOT NULL, 
+    [Class] VARCHAR(50) NOT NULL
+)
---
+++ student_mgnt_system/student_mgnt_system/info.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+namespace student_mgnt_system
+{
+    public partial class info : Form
+    {
+        SqlConnection con = new SqlConnection(@"Data Source=.;Initial Catalog=student_info;Integrated Security=True");
+        public info()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            con.Open();
+            SqlCommand cmd = new SqlCommand("insert into [Table] values('"+textBox1.Text+ "','" + textBox2.Text + "','" + textBox3.Text + "','" + textBox4.Text + "')",con);
+            cmd.ExecuteNonQuery();
+            MessageBox.Show("inserted successfully...!");
+            con.Close();
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            con.Open();
+            SqlCommand cmd = new SqlCommand("update [table] set Name='"+textBox2.Text+"',Address='"+textBox3.Text+"',Class='"+textBox4.Text+"' where ID='"+textBox1.Text+"'",con);
+            cmd.ExecuteNonQuery();
+            MessageBox.Show("updated successfully...!");
+            con.Close();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            con.Open();
+            SqlCommand cmd=new SqlCommand("delete from [Table] where ID='"+textBox1.Text+"' ",con);
+            cmd.ExecuteNonQuery();
+            MessageBox.Show("deleted successfully");
+            con.Close();
+        }
+    }
+}
